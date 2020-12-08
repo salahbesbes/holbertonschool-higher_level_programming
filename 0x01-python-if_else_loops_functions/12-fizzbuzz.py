b35ebf28b06b9d8bd5b5901a6468d2f263738bf8
salah_besbes <salah_besbes@ymail.com>
2020-12-08 16:48:25 +0100

    Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for nb in range(1, 101):
+        if (nb % 15 == 0):
+            print("{}". format("FizzBuzz"), end=" ")
+        elif (nb % 3 == 0):
+            print("{}".format("Fizz"), end=" ")
+        elif (nb % 5 == 0):
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(nb), end=" ")
