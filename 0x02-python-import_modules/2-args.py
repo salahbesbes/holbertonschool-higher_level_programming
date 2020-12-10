46c89cab430b7aeec1af8a1014d769cf45d438a3
salah_besbes <salah_besbes@ymail.com>
2020-12-10 13:32:22 +0100

    print all args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-import sys
+    args = sys.argv
+    length = len(args)
+    if (len(args) == 1):
+        print('{} arguments.'.format(length - 1))
+    else:
+        print('{} arguments:'.format(length - 1))
 
-args = sys.argv
-length = len(args)
-if (len(args) == 1):
-    print('{} arguments.'.format(length - 1))
-else:
-    print('{} arguments:'.format(length - 1))
-
-for i in range(1, len(args)):
-    print("{}: {}".format(i, args[i]))
+    for i in range(1, len(args)):
+        print("{}: {}".format(i, args[i]))
