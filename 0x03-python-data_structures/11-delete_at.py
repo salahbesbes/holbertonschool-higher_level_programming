40bf7e2df43e1961a55ca63032d00e7ff151e108
salahbesbes <salah_besbes@ymail.com>
2020-12-15 21:35:39 +0100

    delete element from list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return my_list
     my_list.remove(my_list[idx])
     return my_list
