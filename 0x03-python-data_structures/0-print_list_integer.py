456d9608c5ecba1d3c7e0ae06f1513823a879b72
salahbesbes <salah_besbes@ymail.com>
2020-12-15 12:35:31 +0100

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-
-    def print_list_integer(my_list=[]):
-        for el in my_list:
-            print("{}".format(el))
-
+def print_list_integer(my_list=[]):
+    for el in my_list:
+        print("{:d}".format(el))
