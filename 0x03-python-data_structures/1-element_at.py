4a799735cb4c0d939a684e37da12114094a7cf84
salahbesbes <salah_besbes@ymail.com>
2020-12-15 15:26:58 +0100

    retrieve an element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    length = len(my_list)
+    if (length == 0):
+        return None
+    if (idx >= 0 and idx <= length):
+        return my_list[idx]
+    else:
+        return None
