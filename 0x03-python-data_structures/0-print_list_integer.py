349cfb44960513d0fb7f60bdd7ad1768c49576c2
salahbesbes <salah_besbes@ymail.com>
2020-12-15 12:27:59 +0100

    first commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for el in my_list:
-        print("{}".format(el))
-
-
 if __name__ == "__main__":
-    print_list_integer()
+
+    def print_list_integer(my_list=[]):
+        for el in my_list:
+            print("{}".format(el))
+
