94e5a228b42dcd611fe683d7bc9b13341000a818
salahbesbes <salah_besbes@ymail.com>
2020-12-17 16:31:05 +0100

    get best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,12 @@
 
 
 def best_score(a_dictionary):
-    if (a_dictionary is None):
+    if (a_dictionary is None or a_dictionary == {}):
         return None
-    scores = [val for key, val in a_dictionary.items()]
-    scores.sort()
-    return scores[-1]
+    best = 0
+    name = ""
+    for key, val in a_dictionary.items():
+        if a_dictionary[key] >= best:
+            best = a_dictionary[key]
+            name = key
+    return name
