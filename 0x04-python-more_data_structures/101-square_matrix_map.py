cc3fe9fa982e45b6d12c2b73ef26b4fe249b178f
salahbesbes <salah_besbes@ymail.com>
2020-12-17 19:49:18 +0100

    init matrix in one line

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [[el ** 2 for el in row] for row in matrix]
+    return list(map(lambda lst: list(map(lambda el: el ** 2, lst)), matrix))
