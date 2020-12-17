5427b7dab90f120aeb27ed43370743ba84a053a9
salahbesbes <salah_besbes@ymail.com>
2020-12-17 19:27:03 +0100

    get weiight average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,9 +5,11 @@ def weight_average(my_list=[]):
     weights = [el[1] for el in my_list]
     tmp = 0.0
     weight_moy = 0.0
+    if (my_list == []):
+        return 0
     for el in weights:
         weight_moy += el
     for tup in my_list:
         tmp += tup[0] * tup[1]
 
-    return float(2.8)
+    return (tmp / weight_moy)
