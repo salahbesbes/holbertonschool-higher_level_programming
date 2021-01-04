c70313b751efbb3d55034ae6ac0d4fdbdd96d60f
salahbesbes <salah_besbes@ymail.com>
2021-01-04 16:30:10 +0100

    execut safe fct

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,10 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(args[0], args[1])
-    except (IndexError, ZeroDivisionError) as err:
+    except IndexError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    except ZeroDivisionError as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return None
     return result
