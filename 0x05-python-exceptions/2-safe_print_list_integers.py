2f587a1208219ea16199c02d7d07fc3055c0bba2
salahbesbes <salah_besbes@ymail.com>
2021-01-04 14:25:33 +0100

    print only int in a random list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    length = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+            length += 1
+        except (ValueError, TypeError):
+            i += 1
+    print()
+    return length
