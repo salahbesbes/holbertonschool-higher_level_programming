ca0d180a4fae39416b104cfde7989f00a91ee9e5
salahbesbes <salah_besbes@ymail.com>
2021-01-05 18:37:25 +0100

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" empty module """
+
+
+class Square:
+    """ private attr size """
+    def __init__(self, size=0):
+        """ init all attributes
+        args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ getter size """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ setter size
+            Args:
+                size(int): size of square
+        """
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ calculate the area of the square """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ print a square of # """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
+
