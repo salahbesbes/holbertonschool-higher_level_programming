67106e46fa8d0c1d985d8efb085616c9f823457e
salahbesbes <salah_besbes@ymail.com>
2021-01-05 18:13:08 +0100

    calculate square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" empty module """
+
+
+class Square:
+    """ private attr size
+    """
+    def __init__(self, size=0):
+        """ init all attributes
+        args:
+            size (int): size of square
+        """
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+            calculate the area of the square
+        """
+        return self.__size * self.__size
