bda5a1fda56515adab0b7f9bd2e7d5be4fcb197b
salahbesbes <salah_besbes@ymail.com>
2021-01-06 23:03:50 +0100

    compare class

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" empty module """
+
+
+class Square:
+    """ private attr size """
+    def __init__(self, size=0):
+        """ init all attributes
+        args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ getter size """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ setter size
+            Args:
+                size(int): size of square
+        """
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ calculate the area of the square """
+        return self.__size * self.__size
+
+    # __lt__ ( < ), __le__ ( <= ), __gt__ ( > ), and __ge__ ( > )
+    def __eq__(self, o) -> bool:
+        return self.__size == o.area()
+
+    def __ne__(self, o) -> bool:
+        return self.area() != o.area()
+
+    def __lt__(self, object):
+        return self.area() < object.area()
+
+    def __le__(self, object):
+        return self.area() <= object.area()
+
+    def __gt__(self, object):
+        return self.area() > object.area()
+
+    def __ge__(self, object):
+        return self.area() >= object.area()
