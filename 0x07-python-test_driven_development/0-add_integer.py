966c102bef02fada4a6e6301d6081e61bd6a8c0e
salahbesbes <salah_besbes@ymail.com>
2021-01-09 19:18:15 +0100

    first test to add fct

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,26 @@
+#!/usr/bin/python3
+""" no module 
+
+
+
+"""
 
 
 def add_integer(a, b=98):
+    """ add 2 given integer
+    
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
 
+    if a + 1 == a or b + 1 == b:  # catch a value like 1e300
+        raise OverflowError
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-
     return a + b
 
