7853492b1d84f73f525223020eb82ae00691c060
salahbesbes <salah_besbes@ymail.com>
2021-01-10 00:16:43 +0100

    test add_int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ no module """
-"""
-add_integer:
-    Checks if parameters are int
-    Returns sum of parameters
-"""
 
 
 def add_integer(a, b=98):
-    """ add 2 given integer
-    
+    """
+    add_integer:
+        Checks if parameters are int
+        Returns sum of parameters
+        
+        raises:
+            TypeError : if a or b is not int or float
     """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
