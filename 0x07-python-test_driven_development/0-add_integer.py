a25cca2a7df63eb07f7560ced806f8d64e682e8d
salahbesbes <salah_besbes@ymail.com>
2021-01-09 23:59:25 +0100

    test add_int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,11 +16,5 @@ def add_integer(a, b=98):
     
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-#    if a + 1 == a or b + 1 == b:  # catch a value like 1e300
-#        raise OverflowError
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
 
