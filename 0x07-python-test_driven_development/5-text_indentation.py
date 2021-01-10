db5f98cdfeaeeaa7b28251bd3202e0f0b52f3a9d
salahbesbes <salah_besbes@ymail.com>
2021-01-11 00:28:59 +0100

    prnt text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ no module """
 
-
-from typing import Text
-
-
 def text_indentation(text):
     
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for i, ch in enumerate(text):
+    for i in range(len(text)):
         if not (text[i] == ' ' and text[i - 1] in [':', '.', '?']):
             print(text[i], end="")
         if text[i] in [':', '.', '?']:
