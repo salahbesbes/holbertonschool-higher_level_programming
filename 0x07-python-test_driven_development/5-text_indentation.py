5e07a4dae24d5646cfd3c9fd4bca2db65a8334a9
salahbesbes <salah_besbes@ymail.com>
2021-01-10 22:40:46 +0100

    prnt text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" no module """
+
+
+def text_indentation(text):
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    line = ""
+    for ch in text:
+        line += ch
+        if ch in ['.', ':', '?']:
+            if line[0] == " ":
+                print(line[1:])
+            else:
+                print(line)
+            line = ""
+            print()
+
