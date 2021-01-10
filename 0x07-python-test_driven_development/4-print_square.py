fca77e659827ebc145a485757c4bfc4d2417cd89
salahbesbes <salah_besbes@ymail.com>
2021-01-10 21:09:34 +0100

    print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,15 @@
 
 
 def print_square(size):
+    """ print square
+
+    Args:
+        size (int): size
+
+    Raises:
+        TypeError: [size must be an integer]
+        ValueError: [size must be >= 0]
+    """
     if type(size) not in [int, float]:
         raise TypeError("size must be an integer")
     if size < 0:
