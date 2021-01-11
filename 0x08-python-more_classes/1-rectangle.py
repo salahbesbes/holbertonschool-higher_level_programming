d1e16142f57265ade74cd2f3b7406c88d6040d24
salahbesbes <salah_besbes@ymail.com>
2021-01-11 19:49:51 +0100

    create rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
         return self.__width
     @width.setter
     def width(self, value):
-        if not isinstance(self.__width, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if self.__width < 0:
             raise ValueError("width must be >= 0")
@@ -25,7 +25,7 @@ class Rectangle:
     
     @height.setter
     def height(self, value):
-        if not isinstance(self.__height, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
