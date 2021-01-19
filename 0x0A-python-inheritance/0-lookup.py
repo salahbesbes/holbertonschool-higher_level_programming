9a94febee6585952dad9d0d3bd4b395e4806b4c4
salahbesbes <salah_besbes@ymail.com>
2021-01-19 19:52:51 +0100

    print attr and methode

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" no module """
 
 
 def lookup(obj):
-    """ search for all attributes in a class """
+    """ search for all attributes in a class 
+    args:
+        obj (obj): ddd
+    """
     return [el for el in dir(obj)]
