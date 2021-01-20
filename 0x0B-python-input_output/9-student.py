ca29e9a53dec15bca94c17afb6cc9ba467149513
salahbesbes <salah_besbes@ymail.com>
2021-01-20 19:02:43 +0100

    to json

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ module """
-import json
 
 
 class Student:
     """description for student class """
 
     def __init__(self, first_name, last_name, age):
+        """ init class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
