927cf45ab59408589d7d24b15023c9803b7aad8f
salahbesbes <salah_besbes@ymail.com>
2021-01-20 23:41:43 +0100

    insert text at a midle of the file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" no module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ insert text """
+    count = 0
+    line = None  # initial count of characters
+    prev = ""
+    with open(filename, 'r+') as f:           # open file for reading an writing
+        while line != "":
+            # place cursor at the correct character location
+            line = f.readline()
+            prev += line
+            if search_string in line:
+                count = f.tell()
+                remainder = f.read()      # store all character afterwards
+                f.seek(0)
+            # insert text and rewrite the remainder
+                f.write(prev + new_string + remainder)
+                f.seek(count)
