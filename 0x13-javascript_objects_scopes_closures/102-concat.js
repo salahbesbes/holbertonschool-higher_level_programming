f6523e26eac9439858e01741343ab6d8e49ffed6
salahbesbes <salah_besbes@ymail.com>
2021-03-23 23:08:48 +0100

    concat 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,30 @@
 #!/usr/bin/node
-'use strict';
+
+/*
 const fs = require('fs').promises;
 
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const dest = process.argv[4];
 
-async function copyFiles (fileA, fileB, dest) {
+async function copyFiles(fileA, fileB, dest) {
   if (!fileA || !fileB || !dest) return;
   try {
     let content = '';
     content += await fs.readFile(fileA);
     content += await fs.readFile(fileB);
     await fs.writeFile(dest, content);
-  } catch (error) {
-    console.error(`Got an error : ${error.message}`);
-    console.log(error);
-  }
+  } catch (error) {}
 }
 
 copyFiles(fileA, fileB, dest);
+
+*/
+
+const fs = require('fs');
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const dest = process.argv[4];
+let content = fs.readFileSync(fileA, 'utf8');
+content += fs.readFileSync(fileB, 'utf-8');
+fs.writeFileSync(dest, tA + tB);
