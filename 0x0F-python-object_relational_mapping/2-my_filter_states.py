aa344b16b74f1df1abe3e67e1de87c8982fb9939
salahbesbes <salah_besbes@ymail.com>
2021-03-27 15:06:45 +0100

    SQLDB print state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+""" print state modul """
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
-stateName = sys.argv[4]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=password,
-                     db=dataBase)
+def print_state():
+    """ print state  """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+    stateName = sys.argv[4]
 
-cur = db.cursor()
-cur.execute('SELECT * FROM states WHERE name = \'{}\''.format(stateName))
-names = cur.fetchall()
-for name in names:
-    print(name)
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=user,
+
+                         passwd=password,
+                         db=dataBase)
+
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name = \'{}\''.format(stateName))
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_state()
