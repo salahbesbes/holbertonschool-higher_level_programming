1d9b249bfa71021dac3b45cf8cc50171a6e409ce
salahbesbes <salah_besbes@ymail.com>
2021-03-27 19:11:20 +0100

    SQLAlchemy create relation

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" create State Class mapped to table """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()  # metadata shared by all classes
+
+
+class City(Base):
+    """ State class represent single row in states table """
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey("states.id"),
+                      nullable=False)
