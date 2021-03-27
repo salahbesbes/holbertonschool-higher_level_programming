0b0b751741c1d9a4b7b47c96865bc6588dc421b8
salahbesbes <salah_besbes@ymail.com>
2021-03-27 15:01:42 +0100

    SQLDB print table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 """
-SelectStates module
+print_table_state module
 """
 import MySQLdb
 import sys
 
 
-def filter_states():
-    """Filters states from database"""
-
-    username = sys.argv[1]
+def print_table_state():
+    """ print table states """
+    user = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    dataBase = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
-                         user=username,
+                         user=user,
                          passwd=password,
-                         db=database
-                         )
+                         db=dataBase)
     cur = db.cursor()
+
     cur.execute("SELECT *\
                 FROM states\
                 WHERE name LIKE BINARY 'N%'\
-                ORDER BY id ASC"
-                )
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+                ORDER BY id ASC")
+
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+
     cur.close()
     db.close()
 
+
 if __name__ == "__main__":
-    filter_states()
+    print_table_state()
