17b7e5842e3b0d6c568beebcf68878d5e75f0990
salahbesbes <salah_besbes@ymail.com>
2021-03-27 15:42:12 +0100

    SQLDB print all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-import MySQLdb
+""" print cities """
 import sys
+import MySQLdb
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user,
-                     passwd=password, db=dataBase)
+def print_all_cities():
+    """ print all cities """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
 
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=user,
+                         passwd=password,
+                         db=dataBase)
 
-cur.execute("SELECT id, name FROM cities ORDER BY id ASC")
-cities = cur.fetchall()
-for city in cities:
-    print(city)
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM cities ORDER BY id ASC")
+    cities = cur.fetchall()
+    for city in cities:
+        print(city)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_all_cities()
