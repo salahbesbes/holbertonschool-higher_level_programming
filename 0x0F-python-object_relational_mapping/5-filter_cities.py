98898be2cf9de15b9576647f8eef2b25bb176e4e
salahbesbes <salah_besbes@ymail.com>
2021-03-27 16:07:11 +0100

    SQLDB filter cities by state name

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,43 @@
 #!/usr/bin/python3
-import MySQLdb
+""" filter cities modules """
 import sys
+import MySQLdb
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
-stateName = sys.argv[4]
 
-db = MySQLdb.connect(host='localhost', user=user,
-                     passwd=password, db=dataBase)
+def filter_cities():
+    """ filter cities by state """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+    stateName = sys.argv[4]
 
-cur = db.cursor()
-cur.execute("SELECT name FROM cities WHERE state_id = \
-            (SELECT id FROM states WHERE name = \'{}\' )"
-            .format(stateName))
-cities = cur.fetchall()
-res = sep = ''
-for city in cities:
-    res += sep + city[0]
-    sep = ', '
-print(res)
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=user,
+                         passwd=password,
+                         db=dataBase)
+
+    cur = db.cursor()
+    # cur.execute("SELECT name FROM cities WHERE state_id = \
+    #             (SELECT id FROM states WHERE name = \'{}\' )"
+    #             .format(stateName))
+
+    cur.execute(f"SELECT cities.name \
+                FROM cities \
+                INNER JOIN states \
+                ON cities.state_id = states.id \
+                WHERE states.name LIKE BINARY %(stateName)s",
+                {'stateName': stateName})
+    cities = cur.fetchall()
+    res = sep = ''
+    for city in cities:
+        res += sep + city[0]
+        sep = ', '
+    print(res)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filter_cities()
