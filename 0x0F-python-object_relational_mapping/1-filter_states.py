26991f4b8c463b18bf45f89e1595b050d191392d
salahbesbes <salah_besbes@ymail.com>
2021-03-27 14:45:45 +0100

    SQLDB print table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,17 +2,27 @@
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=password,
-                     db=dataBase)
+def print_table_state():
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
 
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost',
+                         user=user,
+                         passwd=password,
+                         db=dataBase)
+    cur = db.cursor()
 
-cur.execute('SELECT * FROM states WHERE name LIKE \'N%\' ORDER BY id ASC')
+    cur.execute('SELECT * FROM states WHERE name LIKE \'N%\' ORDER BY id ASC')
 
-names = cur.fetchall()
-for name in names:
-    print(name)
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_table_state()
