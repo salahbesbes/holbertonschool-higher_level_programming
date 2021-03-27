fbe66ad07ae2583d3210a0a65f735b882cc883c1
salahbesbes <salah_besbes@ymail.com>
2021-03-27 16:22:04 +0100

    SQLAlchemy  lists all State objects from states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""task 7
-"""
+""" list all states modules """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
+def list_all_states():
+    """ list all states of the table states """
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(user, password, dataBase),
-                       pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
 
-states = session.query(State)
-for state in states:
-    print('{}: {}'.format(state.id, state.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, password, dataBase),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+
+
+if __name__ == "__main__":
+    list_all_states()
