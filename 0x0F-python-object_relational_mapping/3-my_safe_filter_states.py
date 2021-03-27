1236fb1ccca70dc92becb36610c97284d4826be0
salahbesbes <salah_besbes@ymail.com>
2021-03-27 15:22:03 +0100

    SQLDB execute safe query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
+""" safe query """
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-password = sys.argv[2]
-dataBase = sys.argv[3]
-stateName = sys.argv[4]
-db = MySQLdb.connect(host='localhost', user=user, passwd=password,
-                     db=dataBase)
 
-cur = db.cursor()
-cur.execute("""SELECT * FROM states WHERE name = {} """.format(stateName))
-names = cur.fetchall()
-for name in names:
-    print(name)
+def safe_query():
+    """ execute safe query """
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+    stateName = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=user,
+                         passwd=password,
+                         db=dataBase)
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name = {} """.format(stateName))
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_state()
