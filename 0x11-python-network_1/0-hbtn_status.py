00779c53997abc142bb3f4eeac5c10f9b10d4ce8
salahbesbes <salah_besbes@ymail.com>
2021-04-10 14:24:23 +0200

    get some caracteristique

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,15 @@ from urllib.request import Request, urlopen
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
     req = Request(url)
-    with urlopen(req) as response:
-        content = response.read()
 
-        print("Body response:\n\
-                - type: {}\n\
-                - content: {}\n\
-                - utf8 content: {}\
-            ".format(content.__class__, content, content.decode('ascii')))
+    try:
+        with urlopen(req) as response:
+            content = response.read()
+
+            print("Body response:\n\
+                    - type: {}\n\
+                    - content: {}\n\
+                    - utf8 content: {}\
+                    ".format(content.__class__, content, content.decode('ascii')))
+    except Exception:
+        pass
