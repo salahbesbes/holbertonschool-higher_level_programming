c9987d1a026ed52ce832300740ba3b87dd928ad3
salahbesbes <salah_besbes@ymail.com>
2021-04-10 14:29:47 +0200

    using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,14 @@
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/statuss'
+    url = 'https://intranet.hbtn.io/status'
     req = Request(url)
 
     with urlopen(req) as response:
         content = response.read()
 
         print("Body response:\n\
-                - type: {}\n\
-                - content: {}\n\
-                - utf8 content: {}"
+        - type: {}\n\
+        - content: {}\n\
+        - utf8 content: {}"
               .format(content.__class__, content, content.decode('ascii')))
