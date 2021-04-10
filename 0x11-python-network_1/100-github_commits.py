ca1f180cc45bb5d5676657ff2d01b0869a619bc5
salahbesbes <salah_besbes@ymail.com>
2021-04-10 17:56:54 +0200

    interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" requests module """
+from requests import post, get
+from requests.models import HTTPBasicAuth
+import sys
+if __name__ == "__main__":
+    """     takes your GitHub credentials (username and password)
+            and uses the GitHub API to display your id
+    """
+
+    repo_name = sys.argv[1]
+    username = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        username, repo_name)
+
+    res = get(url)
+    try:
+        result = res.json()
+        for obj in result:
+            print("{}: {}".format(
+                obj.get('sha'),
+                obj.get('commit').get('author').get('name')
+            ))
+    except Exception:
+        print('None')
