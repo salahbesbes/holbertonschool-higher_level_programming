d5ab648f2e678229cbcb5a7d96ecb15bc798c97e
salahbesbes <salah_besbes@ymail.com>
2021-04-10 15:20:48 +0200

    send post request with data

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" urllib module """
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+import sys
+
+if __name__ == "__main__":
+    """     sends a POST request to the passed URL with the email as a parameter,
+            and displays the body of the response (decoded in utf-8)
+    """
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+
+    data = urlencode(values)
+    data = data.encode('ascii')
+
+    req = Request(url, data)
+    with urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('asccii'))
