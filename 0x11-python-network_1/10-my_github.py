0d53e8e007fd0ddc1b34c295371e9a4aa7fe81e5
salahbesbes <salah_besbes@ymail.com>
2021-04-10 17:43:05 +0200

    using github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" requests module """
+from requests import post, get
+from requests.models import HTTPBasicAuth
+import sys
+if __name__ == "__main__":
+    """     takes your GitHub credentials (username and password)
+            and uses the GitHub API to display your id
+    """
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    res = get(url, auth=('username', token))
+    try:
+        print(res.json().get('id'))
+    except Exception:
+        print('None')
