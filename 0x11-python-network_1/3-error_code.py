f7dea68aa1a59fe8b074e26e6e3aaf0990d03259
salahbesbes <salah_besbes@ymail.com>
2021-04-10 15:33:25 +0200

    handle both URLError and HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" urllib module """
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError, URLError
+import sys
+
+if __name__ == "__main__":
+    """     sends a request to the URL and displays the body
+            of the response (decoded in utf-8). and handle ERROR
+    """
+
+    url = sys.argv[1]
+    req = Request(url)
+    try:
+
+        with urlopen(req) as response:
+            the_page = response.read()
+            print(the_page.decode('ascii'))
+
+    except URLError as error:
+        print('Error Code: {}'.format(error.code))
+    except HTTPError as error:
+        print('Error Code: {}'.format(error.code))
