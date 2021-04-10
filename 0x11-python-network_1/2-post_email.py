c74cd0e0998dd5c7bf25099aaf77db5f618c65be
salahbesbes <salah_besbes@ymail.com>
2021-04-10 15:24:01 +0200

    send post request with data

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     req = Request(url, data)
     with urlopen(req) as response:
         the_page = response.read()
-        print(the_page.decode('asccii'))
+        print(the_page.decode('ascii'))
