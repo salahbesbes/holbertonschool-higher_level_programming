b906784eaf0dce1960c0af25af18f446ab51e075
salahbesbes <salah_besbes@ymail.com>
2021-05-04 18:22:55 +0200

    read file asyncrone way

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-/* this is asyncrone => code below this function get executed before the data is printed */
+/*  this is asyncrone => code below this function get executed before the data is printed */
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) console.log(err);
   else console.log(data);
