b8d4a4b44cb6877ebc12db2cc27c39044e29b99a
salahbesbes <salah_besbes@ymail.com>
2021-05-04 17:40:41 +0200

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* read the content of a file  */
-import * as fs from 'fs';
+const fs = require('fs');
 const filePath = process.argv[2];
 
 /* this is asyncrone => code below this function get executed before the data is printed */
