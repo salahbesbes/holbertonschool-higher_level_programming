30e3d6b3b431fcfe80751a91715674058dee011b
salahbesbes <salah_besbes@ymail.com>
2021-05-04 22:50:49 +0200

    create a mapped dict for completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,7 +2,7 @@
 /* using API to print the title film  */
 const request = require('request');
 const url = process.argv[2];
-
+/* create a mapped dict for completed tasks */
 request.get(url, (error, res, body) => {
   if (error) console.log(error);
   else {
