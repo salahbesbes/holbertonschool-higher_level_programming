56423a2d5579631cb236c80b303d68046acaefbf
salahbesbes <salah_besbes@ymail.com>
2021-05-04 19:07:15 +0200

    send a get request with js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* display the status code of a request  */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, res, body) => {
+  if (error) console.log(error);
+  else console.log(res);
+});
